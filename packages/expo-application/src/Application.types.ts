a14ddc7a903c43aa50db7fa8422f3f9a81c299bc
Mateo Guzmán <info@mateoguzman.net>
2024-12-31 16:53:50 +0100

    [ios][application] add missing jsdocs

--- packages/expo-application/src/Application.types.ts
+++ packages/expo-application/src/Application.types.ts
@@ -1,13 +1,32 @@
-// @docsMissing
 /**
+ * Represents the release type of the application.
  * @platform ios
  */
 export enum ApplicationReleaseType {
+  /**
+   * The release type is not determined or specified.
+   */
   UNKNOWN = 0,
+  /**
+   * The application is running on the iOS Simulator, used for testing apps on a Mac without physical devices.
+   */
   SIMULATOR = 1,
+  /**
+   * The application is distributed internally within an organization using Apple's Enterprise Developer Program.
+   */
   ENTERPRISE = 2,
+  /**
+   * The application is installed on devices for development purposes, signed with development credentials.
+   * This distribution method allows testing without access to team distribution identities.
+   */
   DEVELOPMENT = 3,
+  /**
+   * The application is distributed to a limited audience for testing purposes using an Ad Hoc provisioning profile.
+   */
   AD_HOC = 4,
+  /**
+   * The application is officially released and available to the general public through the App Store.
+   */
   APP_STORE = 5,
 }
 
